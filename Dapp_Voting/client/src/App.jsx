94c7a7738c17170650ec89a0c262e36d845561cd
ielboulo <46560616+ielboulo@users.noreply.github.com>
2023-03-21 07:58:04 +0100

    revert

--- Dapp_Voting/client/src/App.jsx
+++ Dapp_Voting/client/src/App.jsx
@@ -1,10 +1,10 @@
 import { EthProvider } from "./contexts/EthContext";
 import Login from "./components/Login";
-/* import AddVoter from "./components/AddVoter";
+import AddVoter from "./components/AddVoter";
 import ProposalPhase from "./components/ProposalPhase.jsx";
 import VotePhase from "./components/VotePhase.jsx";
 import VoteTally from "./components/VoteTally";
-import VoteWinner from "./components/VoteWinner"; */
+import VoteWinner from "./components/VoteWinner"; 
 import StatusWatcher from "./components/StatusWatcher";
 
 import NewMarc from "./components/NewMarc";
@@ -31,26 +31,7 @@ function App() {
           <h1 className="title_phase"> Status Watcher  </h1>
           <StatusWatcher />
           <hr />
-          
-          {/* ----------------------------------- */}
-          <br />
-        <br />
-          <h1 className="title_dapp_1"> Voting DApp </h1>
-          <br/>
-          <h2 className="title_dapp_2"> -- Voter -- </h2>
-          <br/>
-
-          <h1 className="title_phase"> AddProposal </h1>
-          <AddProposal />
-          <hr />
-
-          <h1 className="title_phase"> AddVote </h1>
-          <AddVote />
-          <hr />
-
-
-          {/* --------------------------------------- */}
-
+        
         </div>
       </div>
     </EthProvider>
